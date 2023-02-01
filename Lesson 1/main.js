3932d0240dd4c02146b85947347e4cfc5e977316
Sad228 <mov4ko@gmail.com>
2023-02-01 12:25:25 +0200

    lesson 1 final

---
+++ Lesson 1/main.js
@@ -0,0 +1,62 @@
+let hello = 'hello'
+console.log('hello');
+
+let owu = 'owu'
+console.log('owu');
+
+let com = 'com'
+console.log('com');
+
+let ua = 'ua'
+console.log('ua');
+
+let num1 = 1;
+console.log(num1);
+
+let num2 = 10;
+console.log(num2);
+
+let num3 = -999;
+console.log(num3);
+
+let num4 = 123;
+console.log(num4);
+
+const PI = 3.14;
+console.log(PI);
+
+let num5 = 2.7;
+console.log(num5);
+
+let num6 = 16;
+console.log(num6);
+
+let tru = true;
+console.log(tru);
+
+let fls = false;
+console.log(fls);
+
+let firstName = 'Andriy';
+let middleName = ' Igorovich'
+let lastName = 'Movchko '
+
+let person = (lastName + firstName + middleName)
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+name = prompt('Your Name?');
+console.log(name);
+
+midname = prompt('Your MiddleName?');
+console.log(midname);
+
+age = prompt('Your age?')
+console.log(age);
