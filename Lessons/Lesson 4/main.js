0613158edb58e3e168687c562291fd4f75366647
Andriy-24 <andriymov4ko24@gmail.com>
2023-02-11 20:35:18 +0200

    add4

--- Lessons/Lesson 4/main.js
+++ Lessons/Lesson 4/main.js
@@ -98,5 +98,22 @@ function far(arr){
     document.write(`</ul>`)
 }
 far(primitive)
-
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+function us(mass){
+    for (const mas of mass) {
+        document.write(`<div>${mas.id} - ${mas.name} - ${mas.age}</div>`)
+    }
+}
+let users_2 = [
+    {name: 'vasya', age: 31,id: 1},
+    {name: 'petya', age: 30,id: 2},
+    {name: 'kolya', age: 29,id: 3},
+    {name: 'olya', age: 28,id: 4},
+    {name: 'max', age: 30,id: 5},
+    {name: 'anya', age: 31,id: 6},
+    {name: 'oleg', age: 28,id: 7},
+    {name: 'andrey', age: 29,id: 8},
+    {name: 'masha', age: 30,id: 9},
+    {name: 'olya', age: 31,id: 10},
+    {name: 'max', age: 31,id: 11}];
+us(users_2)
